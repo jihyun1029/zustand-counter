6accebcd6e6953ccdfca7d2a578f445eb427a153
jihyun1029 <parkji2028@naver.com>
2025-04-10 14:54:18 +0900

    Zustand 카운터

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import CountBox from "./components/CountBox";
+import counterStore from "./stores/counterStore";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const [count, setCount] = useState(0);
+    const {count, increase, increaseBy} = counterStore();
+
+    return (
+        <div className="App">
+            <h1>count:{count}</h1>
+            <button onClick={increase}>increase</button>
+            <button onClick={() => increaseBy(10)}>10씩증가</button>
+            <CountBox count={count} />
+        </div>
+    );
 }
 
 export default App;
